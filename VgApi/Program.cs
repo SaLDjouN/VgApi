2c2546cde48caced734195644c57d8231ed2f965
SaLDjouN <vivarcsc@hotmail.com>
2023-07-12 15:47:49 +0200

    maj

--- VgApi/Program.cs
+++ VgApi/Program.cs
@@ -1,27 +1,64 @@
-var builder = WebApplication.CreateBuilder(args);
+using VgApi.Models;
+using VgApi;
 
-// Add services to the container.
+//var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+//// Add services to the container.
 
-var app = builder.Build();
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//var app = builder.Build();
 
-app.UseHttpsRedirection();
+//// Configure the HTTP request pipeline.
+////if (app.Environment.IsDevelopment())
+////{
+////    app.UseSwagger();
+////    app.UseSwaggerUI();
+////}
 
-app.UseAuthorization();
+//app.UseHttpsRedirection();
 
-app.MapControllers();
+//app.UseAuthorization();
 
-app.Run();
+//app.MapControllers();
+
+//app.Run();
 
 Console.WriteLine("COUCOU");
+
+//using (var context = new ExempleContext())
+//{
+//    var genre = new Genre
+//    {
+//        Name = "RPG"
+//    };
+//    var genre2 = new Genre { Name = "Non-RPG" };
+//    context.Genres.Add(genre);
+//    context.Genres.Add(genre2);
+//    context.SaveChanges();
+//}
+
+using(var context = new ExempleContext())
+{
+    context.Games.Add(new Game
+        {
+            Titre = "Final Fantasy VI",
+            Release_Yr = 1992,
+            Rating = 5,
+            GenreID = 2,
+            Description = "très sympa pas comme Emilie"
+    });
+    context.Games.Add(new Game
+    {
+        Titre = "Mario Kart 8",
+        Release_Yr = 2009, 
+        Rating = 4,
+        GenreID = 3,
+        Description = "plutot sympa comme emilie mais bien trop rarement"
+    });
+
+    context.SaveChanges();
+}
